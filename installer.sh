a550a4a7465a29d4aad963add85e5f2a69332df5
WingedSpade3557 <30960419+WingedSpade3557@users.noreply.github.com>
2018-08-18 19:36:43 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,14 +7,15 @@ apt update
 apt upgrade
 apt install wget
 tput setaf 3; echo [2] Installing required packages...
+tput sgr0
 apt install openjdk-8-jre-headless
 tput sgr0
-tput setaf 3; echo [3] Creating required directory...
-tput sgr0
-mkdir Nukkit
-cd Nukkit
-tput sgr0
-tput setaf 3; echo [4] Downloading Nukkit...
+
+
+
+
+
+tput setaf 3; echo [3] Downloading Nukkit...
 tput sgr0
 wget https://ci.nukkitx.com/job/NukkitX/job/Nukkit/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
 wget https://github.com/WingedSpade3557/NukkitXStarter/releases/download/1.0/start.sh
@@ -22,4 +23,4 @@ chmod +x start.sh
 tput sgr0
 tput setaf 2; echo Nukkit Successfully installed. To run it use ./start.sh [tip:use "screen"]
 tput sgr0
-cd /root/Nukkit/
+
