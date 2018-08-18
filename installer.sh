5d4582840391f0f2dcc6b220fef4f55b70a0ffbb
WingedSpade3557 <30960419+WingedSpade3557@users.noreply.github.com>
2018-08-18 12:01:16 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clear
+tput setaf 3; echo [1] Updating...
+tput sgr0
+apt update
+apt upgrade
+tput setaf 3; echo [2] Installing required packages...
+apt install default-jdk
+tput sgr0
+tput setaf 3; echo [3] Creating required directory...
+mkdir Nukkit
+cd Nukkit
+tput sgr0
+tput setaf 3; echo [4] Downloading Nukkit...
+wget https://ci.nukkitx.com/job/NukkitX/job/Nukkit/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
+wget https://github.com/WingedSpade3557/NukkitXStarter/releases/download/1.0/start.sh
+chmod +x start.sh
+tput sgr0
+tput setaf 2; echo Nukkit Successfully installed. To run it use ./start.sh [tip:use "screen"]
